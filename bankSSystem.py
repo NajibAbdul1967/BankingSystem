b79482c8ed10db9e3b572e90458f1ef6233d482b
NajibAbdul1967 <44771772+NajibAbdul1967@users.noreply.github.com>
2018-11-27 14:54:32 +0000

    Add files via upload

---
+++ bankSSystem.py
@@ -0,0 +1,11 @@
+def profile_settings_menu(self):
+        #print the options you have
+         print (" ")
+         print ("Your Profile Settings Options Are:")
+         print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+         print ("1) Update name")
+         print ("2) Print details")
+         print ("3) Back")
+         print (" ")
+         option = int(input ("Choose your option: "))
+         return option
